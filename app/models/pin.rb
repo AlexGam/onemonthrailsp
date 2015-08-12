affd7f3e953346906d0a4a66170b67b0b86d327f
Alexander Gamero <alex19gam@gmail.com>
2015-08-12 15:15:41 -0430

    added bootstrap, stylesheets, stuffs

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,10 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	
 	#may can use , :default_url => "/images/:style/missing.png"
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :large => "600x600", :thumb => "100x100>" }
 	
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	
+	validates :image, presence: true
+  	validates :description, presence: true
 end
